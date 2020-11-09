5da40539f69334dcc437ba7f9e304591ec01abcc
flavio63730 <flavio.ranchon@gmail.com>
2020-11-09 20:41:37 +0100

    create cpp file

--- TP5/src/Converter.java
+++ TP5/src/Converter.java
@@ -1,27 +1,33 @@
+import java.io.PrintStream;
+import java.lang.reflect.Array;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.*;
 
 public class Converter {
-    public static void convertClass(String classJava, String classCpp) throws ClassNotFoundException {
-        Map<String, ArrayList<String>> content = new HashMap<>();
+    public static void convertClass(String classJava, String classCpp, PrintStream out) throws ClassNotFoundException {
+        Map<String, ArrayList<String>> contentConstructors = new HashMap<>();
+        Map<String, ArrayList<String>> contentFields = new HashMap<>();
+        Map<String, ArrayList<String>> contentMethods = new HashMap<>();
+
         Set<String> dependencies = new HashSet<>();
         Set<String> imports = new HashSet<>();
         Class java = Class.forName(classJava);
 
         List<String> motherClasses = new ArrayList<>();
         getExtention(java, classCpp, imports, motherClasses);
-        getFields(java, content, dependencies);
-        getMethods(java, content, dependencies);
-        getConstructor(java, content, dependencies);
+        getConstructor(java, classCpp, contentConstructors, dependencies);
+        getFields(java, contentFields, dependencies);
+        getMethods(java, contentMethods, dependencies);
 
-        displayGuardianStart(classCpp);
-        displayDependencies(dependencies, imports);
-        displayClassStart(java, classCpp, motherClasses);
-        displayContent(content);
-        displayClassEnd(classCpp);
-        displayGuardianEnd(classCpp);
+        displayGuardianStart(classCpp, out);
+        displayDependencies(dependencies, imports, out);
+        displayClassStart(java, classCpp, motherClasses, out);
+        displayContent(contentConstructors, contentFields, contentMethods, out);
+        displayClassEnd(classCpp, out);
+        displayImplementClass(classCpp, contentConstructors, contentMethods, out);
+        displayGuardianEnd(classCpp, out);
     }
 
     private static String convertType(String javaType, Set<String> dependencies) {
@@ -75,7 +81,7 @@ public class Converter {
         }
     }
 
-    private static void getConstructor(Class javaClass, Map<String, ArrayList<String>> content, Set<String> dependencies) {
+    private static void getConstructor(Class javaClass, String classCpp, Map<String, ArrayList<String>> content, Set<String> dependencies) {
         for (Constructor constructor : javaClass.getDeclaredConstructors()) {
             String carry = constructor.toString().split(" ")[0];
 
@@ -87,51 +93,92 @@ public class Converter {
             if (!content.containsKey(carry)) {
                 content.put(carry, new ArrayList<>());
             }
-            content.get(carry).add(constructor.getName() + "(" + String.join(", ", params) + ")");
+            content.get(carry).add(classCpp + "(" + String.join(", ", params) + ")");
         }
     }
 
-    public static void displayGuardianStart(String cpp) {
-        System.out.println("#ifndef " + cpp.toUpperCase() + "_HPP");
-        System.out.println("#define " + cpp.toUpperCase() + "_HPP");
-        System.out.println();
+    public static void displayGuardianStart(String cpp, PrintStream out) {
+        out.println("#ifndef " + cpp.toUpperCase() + "_HPP");
+        out.println("#define " + cpp.toUpperCase() + "_HPP");
+        out.println();
     }
 
-    private static void displayDependencies(Set<String> dependencies, Set<String> imports) {
+    private static void displayDependencies(Set<String> dependencies, Set<String> imports, PrintStream out) {
         for (String dependency : dependencies) {
-            System.out.println("#include <" + dependency + ">");
+            out.println("#include <" + dependency + ">");
         }
         for (String localImport : imports) {
-            System.out.println("#include \"" + localImport + ".hpp\"");
+            out.println("#include \"" + localImport + ".hpp\"");
         }
-        System.out.println();
+        out.println();
     }
 
-    private static void displayClassStart(Class javaClass, String cpp, List<String> motherClasses) {
-        System.out.print("Class " + cpp);
+    private static void displayClassStart(Class javaClass, String cpp, List<String> motherClasses, PrintStream out) {
+        out.print("class " + cpp);
 
         if ( ! motherClasses.isEmpty() ) {
-            System.out.print(" : " + String.join(", ", motherClasses));
+            out.print(" : public " + String.join(", public ", motherClasses));
         }
 
-        System.out.println(" {");
+        out.println(" {");
     }
 
-    private static void displayContent(Map<String, ArrayList<String>> content) {
-        for (Map.Entry<String, ArrayList<String>> entry : content.entrySet()) {
-            System.out.println("\t" + entry.getKey() + ":");
-            for (String method : entry.getValue()) {
-                System.out.println("\t\t" + method + ";");
+    private static void displayContent(Map<String, ArrayList<String>> contentConstructors, Map<String, ArrayList<String>> contentFields, Map<String, ArrayList<String>> contentMethods, PrintStream out) {
+        String[] visibilities = new String[] {"private", "protected", "public"};
+        for (String visibility : visibilities) {
+            if (contentConstructors.containsKey(visibility) || contentFields.containsKey(visibility) || contentMethods.containsKey(visibility)) {
+                out.println("\t" + visibility + ":");
+                for (String fields : contentFields.get(visibility))
+                    out.println("\t\t" + fields + ";");
+                for (String constructor : contentConstructors.get(visibility))
+                    out.println("\t\t" + constructor + ";");
+                for (String method : contentMethods.get(visibility))
+                    out.println("\t\t" + method + ";");
             }
         }
     }
 
-    private static void displayClassEnd(String cpp) {
-        System.out.println("} // " + cpp);
+    private static void displayClassEnd(String cpp, PrintStream out) {
+        out.println("}; // " + cpp);
     }
 
-    public static void displayGuardianEnd(String cpp) {
-        System.out.println();
-        System.out.println("#endif //" + cpp.toUpperCase() + "_HPP");
+    private static void displayImplementClass(String cpp, Map<String, ArrayList<String>> constructors, Map<String, ArrayList<String>> methods, PrintStream out) {
+        String[] visibilities = new String[] {"private", "protected", "public"};
+
+        for (String visibility : visibilities) {
+            out.println();
+            out.println("// " + visibility + " implementation");
+            for (String constructor : constructors.get(visibility)) {
+                String[] split = constructor.split(" ");
+                for (String s : split) {
+                    if (s.contains("(")) {
+                        out.print(cpp + "::");
+                    }
+                    out.print(s + " ");
+                }
+                out.println("{}");
+            }
+            for (String method : methods.get(visibility)) {
+                String[] split = method.split(" ");
+                for (String s : split) {
+                    if (s.contains("(")) {
+                        out.print(cpp + "::");
+                    }
+                    out.print(s + " ");
+                }
+                out.print("{");
+
+                if (!split[0].equals("void")) {
+                    out.print(" return " + split[0] + "(); ");
+                }
+
+                out.println("}");
+            }
+        }
+    }
+
+    public static void displayGuardianEnd(String cpp, PrintStream out) {
+        out.println();
+        out.println("#endif //" + cpp.toUpperCase() + "_HPP");
     }
 }
--- TP5/src/Main.java
+++ TP5/src/Main.java
@@ -1,11 +1,24 @@
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+
 public class Main {
-    public static void main(String[] args) {
-        if (args.length != 1 && args.length != 2) {
+    public static void main(String[] args) throws IOException {
+        if (args.length < 1 || args.length > 3) {
             System.err.println("Il n'y a pas le bon nombre de paramètres.");
+            System.err.println("Exemple d'utilisation : java Main javaClass [cppClass] [--stdout]");
+            return;
         }
 
+        boolean isSecondArgStdout = (args.length == 2 && args[1].equals("--stdout"));
+        boolean isThirdArgStdout = (args.length == 3 && args[2].equals("--stdout"));
+
+        String javaClass = args[0];
+        String cppClass = args.length == 1 || isSecondArgStdout ? args[0] : args[1];
+        PrintStream out = isSecondArgStdout || isThirdArgStdout ? System.out : new PrintStream("cpp/" + cppClass + ".hpp", StandardCharsets.UTF_8);
+
         try {
-            Converter.convertClass(args[0], (args.length != 2) ? args[0] : args[1]);
+            Converter.convertClass(javaClass, cppClass, out);
         } catch (ClassNotFoundException e) {
             System.err.println(e.getMessage());
         }
